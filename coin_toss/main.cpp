2d8d7f60e0bc51c11d88debb8da97796d8139477
alcides sorto <sorto@alcidess-MacBook-Pro.local>
2014-01-30 17:05:18 -0800

    Initial Commit

---
+++ coin_toss/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  coin_toss
+//
+//  Created by alcides sorto on 1/30/14.
+//  Copyright (c) 2014 alcides sorto. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
